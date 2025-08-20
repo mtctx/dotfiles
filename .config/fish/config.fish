fe04a3e1bd0d642977b06ea2543dbc1d1030dd1b
mtctx <me@mtctx.dev>
2025-08-20 18:46:12 +0200

    Removed Helix Alias, added Alias for Updating Mirrors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,7 @@ bind \e\[A history-search-backward
 bind \e\[B history-search-forward
 
 # Aliases
-alias hx helix
+alias updatemirrors "sudo reflector -c Germany -c Switzerland -c Denmark -c Netherlands -c France -c Austria -c Luxembourg -c Belgium  --latest 80 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
 alias cc clear
 alias rmrf "rm -rf"
 alias pacman "sudo pacman"
